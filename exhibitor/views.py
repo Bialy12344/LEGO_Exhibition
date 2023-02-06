a65d7272cd4da55bcde5be7755566d59d251ba98
Bialy12344 <mackowiak.lukasz1@gmail.com>
2023-02-06 21:48:11 +0100

    Added home page, add_user page, created form contain fields author, title, text

--- exhibitor/views.py
+++ exhibitor/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import UserForm
+def index(request):
+    return render(request, 'exhibitor/index.html', {})
+def add_user(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'exhibitor/index.html', {})
+    else:
+        form = UserForm()
+    return render(request, 'exhibitor/add_user.html', {'form': form})
 
-# Create your views here.
